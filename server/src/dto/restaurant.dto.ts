027c1d930c1480730f8e75cf14fb1cd09b300ff7
s-justina <jus.sobcz@gmail.com>
2021-04-12 20:17:03 +0200

    correct string

--- server/src/dto/restaurant.dto.ts
+++ server/src/dto/restaurant.dto.ts
@@ -1,7 +1,7 @@
 // @ts-ignore
 import { IsArray, IsBoolean, IsNumber, IsOptional, IsString, ValidateNested } from 'class-validator';
 import CreateAddressDto from './address.dto';
-import {Socials} from "../interfaces/restaurant.interface";
+import { Socials } from '../interfaces/restaurant.interface';
 
 class CreateRestaurantDto {
   @IsString()
