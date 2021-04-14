b3b32f5eca177c5e31e321d68bd23fdcbef9b2b0
Natalia <59486546+talcia@users.noreply.github.com>
2021-04-14 23:32:41 +0200

    add logout logic

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,35 +1,35 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-interface NavbarProps {
-  isAuth: boolean;
-}
-
-export const Navbar = ({ isAuth }: NavbarProps) => (
-  <nav>
-    <div className="nav-wrapper cyan darken-1 px1">
-      <NavLink to="/" className="brand-logo">
-        Co jemy?
-      </NavLink>
-      <ul className="right hide-on-med-and-down">
-        <li cy-data="home-nav-link">
-          <NavLink to="/">Home</NavLink>
-        </li>
-        {isAuth ? (
-          <>
-            <li>
-              <NavLink to="/favorite">Favorite</NavLink>
-            </li>
-            <li>
-              <NavLink to="/logout">Logout</NavLink>
-            </li>
-          </>
-        ) : (
+export const Navbar = () => {
+  const isAuth = !!document.cookie.split('=')[1];
+  console.log(document.cookie.split('=')[1]);
+  return (
+    <nav>
+      <div className="nav-wrapper cyan darken-1 px1">
+        <NavLink to="/" className="brand-logo">
+          Co jemy?
+        </NavLink>
+        <ul className="right hide-on-med-and-down">
           <li cy-data="home-nav-link">
-            <NavLink to="/login">Login</NavLink>
+            <NavLink to="/">Home</NavLink>
           </li>
-        )}
-      </ul>
-    </div>
-  </nav>
-);
+          {isAuth ? (
+            <>
+              <li>
+                <NavLink to="/favorite">Favorite</NavLink>
+              </li>
+              <li>
+                <NavLink to="/logout">Logout</NavLink>
+              </li>
+            </>
+          ) : (
+            <li cy-data="home-nav-link">
+              <NavLink to="/login">Login</NavLink>
+            </li>
+          )}
+        </ul>
+      </div>
+    </nav>
+  );
+};
